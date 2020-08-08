e180d2ba9ee7c45472229a038ab91726fe60639c
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-08 01:59:19 -0400

    add fetchdata function and renderoption

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,3 +1,21 @@
-it('works', () => {
+it('Shows an autocomplete', () => {
+  createAutoComplete({
+    root: document.querySelector('#target'),
+    fetchData() {
+      return [
+        { Title: 'Avengers' },
+        { Title: 'Not Avengers' },
+        { Title: 'Other Movie' },
+      ];
+    },
+    renderOption(movie) {
+      return movie.Title;
+    },
+  });
+
+  const dropdown = document.querySelector('.dropdown');
+
   
+
+
 });
